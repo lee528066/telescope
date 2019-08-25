95bc07e999bb9a516fe010d25bd2bb19336bd5d3
lee <494037524@qq.com>
2019-08-26 00:31:44 +0800

    提交用于测试datax的kafkawriter插件

--- telescope-core/src/main/java/com/lee/ts/consumer/HellbikeDataxConsumer.java
+++ telescope-core/src/main/java/com/lee/ts/consumer/HellbikeDataxConsumer.java
@@ -1,7 +1,7 @@
 package com.lee.ts.consumer;
 
-import com.lee.ts.event.caster.EventMultiCaster;
 import com.lee.ts.event.SyncEvent;
+import com.lee.ts.event.caster.EventMultiCaster;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,21 +13,22 @@ import javax.annotation.Resource;
 import java.util.List;
 
 /**
+ * 临时用于测试datax 自己实现的kafkawriter插件逻辑
  * @author liwei
  */
 @Slf4j
 @Component
-public class MaxwellConsumer implements BatchAcknowledgingMessageListener<String, String> {
+public class HellbikeDataxConsumer implements BatchAcknowledgingMessageListener<String, String> {
 
     @Resource
     private EventMultiCaster eventMulticaster;
 
     @Override
-    @KafkaListener(containerFactory = "kafkaListenerContainerFactory", groupId = "telescope.maxwell.consumer", topics = "maxwell", autoStartup = "true")
+    @KafkaListener(containerFactory = "kafkaListenerContainerFactory", groupId = "telescope.datax.consumer", topics = "student_sync_topic", autoStartup = "true")
     public void onMessage(List<ConsumerRecord<String, String>> dataRecords, Acknowledgment acknowledgment) {
         for (ConsumerRecord<String, String> dataRecord : dataRecords) {
             log.info("key:{} message:{}", dataRecord.key(), dataRecord.value());
-            eventMulticaster.publish(new SyncEvent(this, dataRecord.value()));
+            //eventMulticaster.publish(new SyncEvent(this, dataRecord.value()));
         }
         acknowledgment.acknowledge();
     }
