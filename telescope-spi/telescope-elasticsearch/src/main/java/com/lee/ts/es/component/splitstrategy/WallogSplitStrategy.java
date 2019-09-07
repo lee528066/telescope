fc3ad61e60bc5d92b1a54da16e49c8c05943fefd
liwei03 <liwei03@corp.netease.com>
2019-09-07 17:01:29 +0800

    WallogSplitStrategy加注解

--- telescope-spi/telescope-elasticsearch/src/main/java/com/lee/ts/es/component/splitstrategy/WallogSplitStrategy.java
+++ telescope-spi/telescope-elasticsearch/src/main/java/com/lee/ts/es/component/splitstrategy/WallogSplitStrategy.java
@@ -1,9 +1,13 @@
 package com.lee.ts.es.component.splitstrategy;
 
+import org.springframework.stereotype.Component;
+
 /**
+ * postgresql
  * @author liwei
  * @date 2019-09-07 15:47
  */
+@Component("wallogSplitStrategy")
 public class WallogSplitStrategy extends AbstractIndexSplitStrategy {
 
     private final static String PREFIX = "postgresqlwallog-";
